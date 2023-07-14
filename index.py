2928db0dccc215772b453fbaf9384ff7cb5648eb
Stephanoslangate <nzoux01@gmail.com>
2023-07-14 13:07:54 +0200

    first commit V4 M2gl team-stephanos

--- index.py
+++ index.py
@@ -1,26 +1,19 @@
-from flask import Flask, render_template
-import csv
-from sklearn import model_selection
+from flask import Flask, render_template,request, redirect,jsonify
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def starpage():
+    #print('The scikit-learn version is {}.'.format(sklearn.__version__))
     return render_template('google-analytics-revenue-prediction.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    with open('submission.csv', 'r') as fichier:
-        lecteur_csv = csv.reader(fichier)
-        data = list(lecteur_csv)
-
-    # Effectuer la prédiction en utilisant votre modèle
-    prediction = model_selection.predict(data)
-    print(prediction)
-
+        return "Success"
     # Retourner la prédiction sous forme de réponse JSON
-    # return jsonify(prediction)
+    #return jsonify(prediction)
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+    app.run()
