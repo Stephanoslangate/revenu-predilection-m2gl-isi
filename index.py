d3ce51b7960620fcb70a207ce14a3a2df4afd036
Stephanoslangate <nzoux01@gmail.com>
2023-07-14 12:32:09 +0200

    first commit V3 M2gl team-stephanos

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import csv
+from sklearn import model_selection
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +9,18 @@ def starpage():
     return render_template('google-analytics-revenue-prediction.html')
 
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    with open('submission.csv', 'r') as fichier:
+        lecteur_csv = csv.reader(fichier)
+        data = list(lecteur_csv)
 
+    # Effectuer la prédiction en utilisant votre modèle
+    prediction = model_selection.predict(data)
+    print(prediction)
+
+    # Retourner la prédiction sous forme de réponse JSON
+    # return jsonify(prediction)
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000)
